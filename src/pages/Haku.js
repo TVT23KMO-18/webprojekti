e0b1ea5248bd56a94e5f78389fb0f4c7beb6ca62
JaLuigi <t2luja00@students.oamk.fi>
2024-04-10 14:06:09 +0300

    Päivitty haku

--- src/pages/Haku.js
+++ src/pages/Haku.js
@@ -3,53 +3,59 @@ import axios from 'axios';
 import "./Haku.css";
 
 export default function Haku() {
-  const [movies, setMovies] = useState([]);
+  const [media, setMedia] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
   const [year, setYear] = useState('');
   const [years, setYears] = useState([]);
   const [genre, setGenre] = useState('');
   const [genres, setGenres] = useState([]);
- 
+  const [mediaType, setMediaType] = useState('movie');
+  const [error, setError] = useState('');
 
-  const getMovies = async () => {
+  const getMedia = async () => {
     try {
+      setLoading(true);
+      setError('');
       const apiKey = process.env.REACT_APP_API_KEY;
-      let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`;
+      let url = `https://api.themoviedb.org/3/search/${mediaType}?api_key=${apiKey}&query=${searchQuery}`;
+  
       if (searchQuery === '') {
-        url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}&primary_release_year=${year}`;
+        url = `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${apiKey}&with_genres=${genre}&primary_release_year=${year}`;
       }
-
-      let allMovies = [];
+  
+      let allMedia = [];
       let page = 1;
       let totalPages = 1;
   
       while (page <= totalPages) {
         const response = await axios.get(`${url}&page=${page}`);
-        allMovies = [...allMovies, ...response.data.results];
+        allMedia = [...allMedia, ...response.data.results];
         totalPages = response.data.total_pages;
-        if (page >= response.data.total_pages || allMovies.length >= 100) {
+        if (page >= response.data.total_pages || allMedia.length >= 200) {
           break;
         }
         page++;
       }
   
-      setMovies(allMovies);
-      setLoading(false);
+      setMedia(allMedia);
     } catch (err) {
-      console.error('Error fetching movies: ', err);
+      console.error('Error fetching media: ', err);
+      setError('Error fetching media. Please try again later.');
+    } finally {
       setLoading(false);
     }
-  };
+  };  
 
   const getGenres = async () => {
     try {
       const apiKey = process.env.REACT_APP_API_KEY;
-      const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
+      const url = `https://api.themoviedb.org/3/genre/${mediaType}/list?api_key=${apiKey}`;
       const response = await axios.get(url);
       setGenres(response.data.genres);
     } catch (err) {
       console.error('Error fetching genres: ', err);
+      setError('Error fetching genres. Please try again later.');
     }
   };
 
@@ -60,20 +66,25 @@ export default function Haku() {
       setYears(yearsArray);
     } catch (err) {
       console.error('Error fetching years: ', err);
+      setError('Error fetching years. Please try again later.');
     }
   };
 
   useEffect(() => {
     getGenres();
     getYears();
-  }, []);
+  }, [mediaType]);
 
   useEffect(() => {
-    getMovies();
-  }, [searchQuery, year, genre]);
+    getMedia();
+  }, [searchQuery, year, genre, mediaType]);
 
   const handleInputChange = (event) => {
     setSearchQuery(event.target.value);
+  };
+
+  const handleMediaTypeChange = (event) => {
+    setMediaType(event.target.value);
   };
 
   const handleGenreChange = (event) => {
@@ -85,30 +96,37 @@ export default function Haku() {
   };
 
   const handleClick = () => {
-    getMovies();
+    getMedia();
   };
 
   return (
     <div>
-      <div>
-        <input value={searchQuery} onChange={handleInputChange} placeholder="Search for a movie"/>
+      <div className='painike'>
+        <input value={searchQuery} onChange={handleInputChange} placeholder="Etsi elokuva tai sarja"/>
+        <select value={mediaType} onChange={handleMediaTypeChange}>
+          <option value="movie">Elokuva</option>
+          <option value="tv">Sarja</option>
+        </select>
         <select value={genre} onChange={handleGenreChange}>
-          <option value="">Select Genre</option>
+          <option value="">Valitse genre</option>
           {genres.map(genre => (
             <option key={genre.id} value={genre.id}>{genre.name}</option>
           ))}
         </select>
         <select value={year} onChange={handleYearChange}>
-          <option value="">Select Year</option>
+          <option value="">Valitse vuosi</option>
           {years.map((yr, index) => (
             <option key={index} value={yr}>{yr}</option>
           ))}
         </select>
-        <button onClick={handleClick}>Search</button>
+        <button onClick={handleClick} disabled={loading}>{loading ? 'Lataa...' : 'Haku'}</button>
       </div>
-      <div>
-        {movies.map((movie) => (
-           <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
+      {error && <div>{error}</div>}
+      <div className='elokuvaKansi'>
+        {media.map((item) => (
+        <div key={item.id}>
+          <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.title || item.name} />
+        </div>
         ))}
       </div>
     </div>
